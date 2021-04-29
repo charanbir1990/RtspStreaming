b69fd1779d100830b9327ac716d6f121a97d8729
Charanbir Singh <charanbir1990@gmail.com>
2021-04-29 16:34:47 +0530

    first commit

--- RTSP/RTSP/ViewController.swift
+++ RTSP/RTSP/ViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scrollVw: UIScrollView!
     var video: RTSPPlayer!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        scrollVw.minimumZoomScale = 1.0
+        scrollVw.maximumZoomScale = 10.0
+        scrollVw.zoomScale = 1.0
         video = RTSPPlayer(video: "rtsp://admin:L254A730@192.168.0.101:554/cam/realmonitor?channel=1&subtype=0", usesTcp: true)
         if video == nil {return}
         video.outputWidth = 1920 //Int32(UIScreen.main.bounds.width)
@@ -42,3 +45,8 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+}
