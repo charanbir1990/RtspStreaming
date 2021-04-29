68350f76fd7cf9b5a5f4ff6246015777d866ea75
Charanbir Singh <charanbir1990@gmail.com>
2021-04-29 16:11:05 +0530

    first commit

---
+++ RTSP/RTSP/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  RTSP
+//
+//  Created by Charanbir Singh on 29/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    var video: RTSPPlayer!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        video = RTSPPlayer(video: "rtsp://admin:L254A730@192.168.0.101:554/cam/realmonitor?channel=1&subtype=0", usesTcp: true)
+        if video == nil {return}
+        video.outputWidth = 1920 //Int32(UIScreen.main.bounds.width)
+        video.outputHeight = 1080 //Int32(UIScreen.main.bounds.height)
+        video.seekTime(0.0)
+        
+        let timer = Timer.scheduledTimer(timeInterval: 1.0/30.0, target: self, selector: #selector(ViewController.update), userInfo: nil, repeats: true)
+        timer.fire()
+    }
+    
+  @objc func update(timer: Timer) {
+        if(!video.stepFrame()){
+            timer.invalidate()
+            video.closeAudio()
+        }
+        imageView.image = video.currentImage
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
